6d329be0845fb94c1ab70b3aa35807f06c4915de
Shunto Shirasawa <2211140041j@ed.fuk.kindai.ac.jp>
2024-11-06 08:42:06 +0000

    xx

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  
+  config.hosts << "e1ccc1d9970a4b74b1124fea61841802.vfs.cloud9.us-east-1.amazonaws.com"
 end

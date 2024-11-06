7f7cf4c21ea16b947e23a832536898a2b1fb870f
TSUTSUMI RYUTA <2211140060x@ed.fuk.kindai.ac.jp>
2024-11-06 08:35:37 +0000

    u

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  
+  config.hosts << "fdff922b9c5848ab9588ed2f8091c373.vfs.cloud9.us-east-1.amazonaws.com"
 end

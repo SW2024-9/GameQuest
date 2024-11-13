ec818a21e9d89cd16303828c44391370c3688e0b
Shunto Shirasawa <2211140041j@ed.fuk.kindai.ac.jp>
2024-11-13 08:18:52 +0000

    createuser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+  
+  def create
+    signup_password = BCrypt::Password.create(params[:user][:pass])
+    u = User.new(uid: params[:user][:uid], pass: signup_password)
+    u.save
+    redirect_to users_parh
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-<<<<<<< HEAD
-  resources :tweets
   get 'top/main'
   post 'top/login'
   get 'top/logout'
   
   root 'top#main'
   
-  resources :likes
-=======
-  root 'users#index'
->>>>>>> bb0a7aa0706dd33b304a56d3834bff04f33b7754
 end
